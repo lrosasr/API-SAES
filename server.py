18bdf42881f2d572a1d2fdb235dff4877dc9321f
lrosasr <128667775+lrosasr@users.noreply.github.com>
2023-08-28 15:12:35 -0600

    modificada la transision login.html -> editar.html

--- server.py
+++ server.py
@@ -22,14 +22,14 @@ class _fila:
 	def generarID(self, n=3):
 		q = ""
 		for o in range(0,n):
-			p = randint(42,57) if randint(0,1) % 2 == 0 else randint(97,122) if randint(0,1) % 2 == 1 else randint(63, 90)
+			p = randint(48,57) if randint(0,1) % 2 == 0 else randint(97,122) if randint(0,1) % 2 == 1 else randint(65, 90)
 			q += chr(p)
 		return q
 	def agregar(self):
 		print("add")
 		self.update()
 		self.gID += 1
-		id = self.generarID(10)
+		id = self.generarID(18)
 		self.clientes[id] = [self.gID, time.time()]
 		return id
 
@@ -44,13 +44,16 @@ class _fila:
 
 	def update(self):
 		#TODO: Escribir CORRECTACTAMENTE esta rutina (el dict puede ser modificado mientras es iterado, no no no)
-		eph = time.time()-10
+		eph = time.time()
 		tmp = [0, 0, 0] #ordenar, apartir de, restar
+		tmp_rm = [] #guardar las keys de los cliente a borrar
 		for cliente in self.clientes:
-			if self.clientes[cliente][1] <= eph: #quitar a los que su epoch sea mas antiguo a 10 seg
+			if eph-self.clientes[cliente][1] >= 10: #quitar a los que su epoch sea mas antiguo a 10 seg
 				self.gID -= 1
 				tmp = [1, self.clientes[cliente][0], tmp[2]+1] if (self.clientes[cliente][0] < tmp[1]) else [1, tmp[1], tmp[2]+1]
-				del self.clientes[cliente]
+				tmp_rm.append(cliente)
+		for i in tmp_rm: #borrar los clientes marcados para eliminar
+			del self.clientes[i]
 		if tmp[0]:
 			for cliente in self.clientes:
 				if self.clientes[cliente][0] >= tmp[1]:
@@ -72,7 +75,7 @@ def index_login():
 		if(request.headers.get("Content-Type") == "application/json"):
 			data = request.json
 		else:
-			return jsonify({"err":1, "txt":"Request invalido"}),  #WTF?
+			return jsonify({"err":1, "txt":"Request invalido"}), 520#WTF?
 		if "id" in data:
 			if not (data.get("id") in fila.clientes):
 				return jsonify({"err":1, "txt":"Tiempo limite de espera alcanzado. Intentalo de nuevo."}), 530
@@ -98,12 +101,16 @@ def index_login():
 					if(not (nav.login(boleta=data.get("boleta"), password=data.get("password"), captcha=data.get("captcha")))):
 						fila.eliminar(data.get("id"))
 						return jsonify({"error":nav.errorMsg}), 506
-					#TODO: escribir en API/webscr.py>main la rutina para extraer la info necesaria
 					d = nav.leer_datos()
-					r = quote(render_template('editar.html',\
-						nombre=d[0],boleta=d[1],telefono=d[2],mail=d[3],ingreso_a=d[1][0:4], total_creditos=d[7],\
-						acreditadas=d[6]))
-					return jsonify({"html":r}), 200
+					#r = quote(render_template('editar.html',\
+					#	nombre=d[0],boleta=d[1],telefono=d[2],mail=d[3],ingreso_a=d[1][0:4], total_creditos=d[7],\
+					#	acreditadas=d[6]))
+					fila.clientes[data.get("id")].append(\
+						render_template('editar.html',\
+							nombre=d[0],boleta=d[1],telefono=d[2],mail=d[3],ingreso_a=d[1][0:4], total_creditos=d[7],\
+							acreditadas=d[6]))
+					#return jsonify({"html":r}), 200
+					return jsonify({"html":"ok"}), 200
 				case _ :
 					print("default")
 					return ""
@@ -114,10 +121,15 @@ def index_login():
 		return "???", 666
 	return ""
 
-@app.route("/hoja")
-def hoja_en_blanco(): #enviar al navegador un editar.html en blanco
-	return render_template("editar.html", \
-	nombre="",boleta="",telefono="",num_periodos=0) 
+@app.route("/hoja/<cliente_id>", methods = ['GET'])
+def hoja_inscripcion(cliente_id):
+	if cliente_id in fila.clientes:
+		render_template_temp = fila.clientes[cliente_id][2]
+		fila.eliminar(cliente_id)
+		return render_template_temp
+	else:
+		return render_template("editar.html", \
+		nombre="",boleta="",telefono="",num_periodos=0) 
 
 @app.route('/')
 def pagina_principal():
